f71c0327d17ba8c9ade1d67dff14bae726cb74b3
Trevor Gross <tmgross@umich.edu>
2023-08-24 01:56:20 -0400

    Add macro implementations for arrays

--- altium/src/__private.rs
+++ altium/src/__private.rs
@@ -7,13 +7,3 @@ use crate::{common::buf2lstring, logging::UNSUPPORTED_KEYS};
 pub fn num_unsupported_keys() -> u32 {
     UNSUPPORTED_KEYS.load(Ordering::Relaxed)
 }
-
-/// Called from our proc macro to log a key
-pub fn macro_unsupported_key(name: &str, key: &[u8], val: &[u8]) {
-    UNSUPPORTED_KEYS.fetch_add(1, Ordering::Relaxed);
-    eprintln!(
-        "unsupported key for `{name}`: {}={} (via `FromRecord` derive)",
-        buf2lstring(key),
-        buf2lstring(val)
-    );
-}
