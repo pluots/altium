c189bfa21904c002364b9ec911c75151feec8004
Trevor Gross <tmgross@umich.edu>
2024-06-06 13:54:09 -0500

    Add support for more file types in the CLI; improve filtering

--- ecad-cli/src/cli.rs
+++ ecad-cli/src/cli.rs
@@ -11,12 +11,19 @@ pub struct Args {
 pub enum Subcommand {
     #[command(subcommand, alias = "sl")]
     Schlib(CmdSchlib),
+    #[command(subcommand, alias = "s")]
+    Schdoc(CmdSchdoc),
     #[command(subcommand, alias = "pl")]
     Pcblib(CmdPcblib),
 }
 
 #[derive(Debug, clap::Subcommand)]
 pub enum CmdSchlib {
+    List(LibListArgs),
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum CmdSchdoc {
     List(LibListArgs),
 }
 
@@ -32,10 +39,19 @@ pub struct LibListArgs {
     /// Single item
     #[arg(short, long)]
     pub item: Vec<String>,
+    /// Match items with the given regular expression
+    #[arg(short = 'E', long)]
+    pub item_re: Vec<String>,
+
     /// List records for the items
     #[arg(short, long, default_value_t = false)]
     pub records: bool,
 
-    #[arg(short, long, value_delimiter = ',')]
-    pub filter: Vec<String>,
+    /// Filter records (matched as regex)
+    #[arg(short = 'f', long, value_delimiter = ',')]
+    pub record_filter: Vec<String>,
+
+    /// Only show these fields from the records (matched as regex)
+    #[arg(short = 'd', long, value_delimiter = ',')]
+    pub field_filter: Option<String>,
 }
